b3e8d5c089d4c5e9d775061ec2830549945db071
ltlemonjuice <luca.fluri4@gmail.com>
2017-08-29 03:59:08 +0200

    blablabla

--- Arduino/ServoTester/ServoTester/ServoTester.ino
+++ Arduino/ServoTester/ServoTester/ServoTester.ino
@@ -2,6 +2,10 @@
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <string>
+#include "ESP8266WiFi.h"
+#include <Wire.h>
+#define WIRE Wire
 
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
@@ -15,18 +19,30 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  100 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
 
+//Converts deg (0-180) to pulsewidth between min and max
+int pulse(int deg){
+  return map(deg, 0, 180, SERVOMIN, SERVOMAX);
+}
+
 
 //DEFAULT Angles (Rest Position):
 //0: 1°   1: 1°   2: 90°
 //3: 65°  4: 70°  5: 1°
+//Angles for all servos
+int defPos[6] = {1, 1, 90, 65, 70, 1};
+int pos[6] = {1, 1, 90, 65, 70, 1};
+
 
 
 // our servo # counter
 uint8_t servonum = 0;
 
+
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("Servo Test on Servo #");
+  Serial.setDebugOutput(true);
+  Serial.print("Servo Test on Servo #");
   Serial.println(servonum);
 
   pwm.begin();
@@ -35,86 +51,74 @@ void setup() {
 
   yield();
   resetPos();
+  wifi();
 
 }
 
+void wifi(){
+  const char* ssid = "WOKO";
+  const char* password = "wlan-4-woko";
+
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("nConnecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+
+  //Successful Connect:
+  nod();
+}
+
+
+void nod(){
+  pwm.setPWM(4, 0, pulse(pos[4]+20));
+  delay(250);
+  pwm.setPWM(4, 0, pulse(pos[4]));
+}
+
 void resetPos() {
-  pwm.setPWM(0, 0, map(1, 0, 180, 100, 600));
+  pwm.setPWM(0, 0, pulse(defPos[0]));
   delay(10);
-  pwm.setPWM(1, 0, map(1, 0, 180, 100, 600));
+  pwm.setPWM(1, 0, pulse(defPos[1]));
   delay(10);
-  pwm.setPWM(2, 0, map(90, 0, 180, 100, 600));
+  pwm.setPWM(2, 0, pulse(defPos[2]));
   delay(10);
-  pwm.setPWM(3, 0, map(65, 0, 180, 100, 600));
+  pwm.setPWM(3, 0, pulse(defPos[3]));
   delay(10);
-  pwm.setPWM(4, 0, map(70, 0, 180, 100, 600));
+  pwm.setPWM(4, 0, pulse(defPos[4]));
   delay(10);
-  pwm.setPWM(5, 0, map(50, 0, 180, 100, 600));
+  pwm.setPWM(5, 0, pulse(defPos[5]));
 }
 
 
 void loop() {
 
-  uint16_t oldInput = 0;
-  uint16_t input = Serial.parseInt();
-  if(input == 900){
+  while(Serial.available()==0){}
+
+  String input = Serial.readString();
+
+  if(input=="reset"){
     resetPos();
-  }
-
-  if(input == 990){
-    servonum = 0;
-    Serial.println("Servo #");
-    Serial.print(servonum);
-    input = Serial.parseInt();
-  }
-  if(input == 991){
-    servonum = 1;
-    Serial.println("Servo #");
-    Serial.print(servonum);
-    input = Serial.parseInt();
-  }
-  if(input == 992){
-    servonum = 2;
-    Serial.println("Servo #");
-    Serial.print(servonum);
-    input = Serial.parseInt();
-  }
-  if(input == 993){
-    servonum = 3;
-    Serial.println("Servo #");
-    Serial.print(servonum);
-    input = Serial.parseInt();
-  }
-  if(input == 994){
-    servonum = 4;
-    Serial.print("Servo #");
-    Serial.println(servonum);
-    input = Serial.parseInt();
-  }
-  if(input == 995){
-    servonum = 5;
-    Serial.println("Servo #");
-    Serial.print(servonum);
-    input = Serial.parseInt();
-  }
-
-  if(input == 999){
-    Serial.println("Select Servo 0-7");
-    while(Serial.available() == 0){}
-    input = Serial.parseInt();
-    if(input < 8 && input >= 0 && input != servonum){
-      servonum = input;
-      Serial.println("Servo Test on Servo #");
-      Serial.print(servonum);
-      input = Serial.parseInt();
-    }
-
-  }
-  if(input != oldInput){
     Serial.println(input);
-    input = map(input, 0, 180, 100, 600);
-    pwm.setPWM(servonum, 0, input);
-    delay(10);
-    oldInput = input;
+  }
+  if(input=="nod"){
+    nod();
+    Serial.println("Nodding...");
+  }
+  if(input.substring(0, 3)=="get"){
+    Serial.println(pos[input.substring(4).toInt()]);
+  }
+  // e.g. "5 145"
+  else{
+    servonum = input.substring(0, 1).toInt();
+    pwm.setPWM(servonum, 0, pulse(input.substring(2).toInt()));
+    pos[servonum] = input.substring(2).toInt();
+    Serial.println(input);
+    Serial.println(pos[servonum]);
   }
 }
